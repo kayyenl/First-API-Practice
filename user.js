ca786389dc14dfe24100132fc2f10c3ad856b820
kayyenl <yenyenlee9@gmail.com>
2022-07-14 23:15:25 +0800

    refactored code, almost done

--- user.js
+++ user.js
@@ -1,8 +1,17 @@
 const postListElem = document.querySelector('.post-list')
 
- async function main() {
+ function main() {
     const idNumber = localStorage.getItem("id")
-    const userPosts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${idNumber}`);
+    displayPosts(idNumber)
+ }
+
+ function onSearchChange(event) {
+    displayPosts(event.target.value)
+ }
+
+
+ async function displayPosts(id) {
+    const userPosts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
     const userPostsToJson = await userPosts.json();
 
     postListElem.innerHTML = userPostsToJson.map(post => {
@@ -16,22 +25,5 @@ const postListElem = document.querySelector('.post-list')
       </div>`
     }).join("")
  }
-
- async function onSearchChange(event) {
-    console.log(event)
-    const userPosts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${event.target.value}`);
-    const userPostsToJson = await userPosts.json();
-
-    postListElem.innerHTML = userPostsToJson.map(post => {
-        return  `<div class="post">
-        <div class="post__title">
-          ${post.title}
-        </div>
-        <p class="post__body">
-          ${post.body}
-        </p>
-      </div>`
-    }).join("")
- }
-
+ 
  main()
