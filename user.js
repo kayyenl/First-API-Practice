05c0c039ee82b56f665dd98484d67763a3105ba9
kayyenl <yenyenlee9@gmail.com>
2022-07-14 22:45:23 +0800

    successfully implemented the posts changing

--- user.js
+++ user.js
@@ -17,4 +17,21 @@ const postListElem = document.querySelector('.post-list')
     }).join("")
  }
 
+ async function onSearchChange(event) {
+    console.log(event)
+    const userPosts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${event.target.value}`);
+    const userPostsToJson = await userPosts.json();
+
+    postListElem.innerHTML = userPostsToJson.map(post => {
+        return  `<div class="post">
+        <div class="post__title">
+          ${post.title}
+        </div>
+        <p class="post__body">
+          ${post.body}
+        </p>
+      </div>`
+    }).join("")
+ }
+
  main()
