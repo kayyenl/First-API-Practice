9fd98a77412914e73137f847f223739dd15070de
kayyenl <yenyenlee9@gmail.com>
2022-07-14 19:38:45 +0800

    made the home page

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
+const userListElem = document.querySelector(".user-list")
+
 async function main() {
     const users = await fetch('https://jsonplaceholder.typicode.com/users')
-    let usersData = await  users.json() //we cannot run this line and the line below it in different orders!
-    console.log(usersData)
-    usersData = usersData.map(user => {
-        `<div class="user-card">
-            <div class="user-card__container">
-                <h3>${user.name}</h4>
-                <p><b>Email:</b>${user.email}</p>
-                <p><b>Phone:</b>${user.phone}</p>
-                <p><b>Website:</b> <a href=>${user.website} target="_blank">${user.website}</a></p>
-            </div>  
-        </div>`
-    }).join("")
-    document.querySelector(".user").innerHTML += usersData;
+    const usersData = await users.json() //we cannot run this line and the line below it in different orders!
+    
+    userListElem.innerHTML = usersData.map((user) => userHTMLString(user)).join("")
+}
+
+function userHTMLString(user) {
+    return `<div class="user-card">
+    <div class="user-card__container">
+        <h3>${user.name}</h4>
+        <p><b>Email:</b>${user.email}</p>
+        <p><b>Phone:</b>${user.phone}</p>
+        <p><b>Website:</b> <a href= "https://${user.website}" target="_blank">${user.website}</a></p>
+    </div>  
+</div>`
 }
 
 main();
